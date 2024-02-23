66d9d7c6675dfdcffde11a1265c068f21984e9ba
SzymonNiemiec <saymek@o2.pl>
2024-02-23 15:59:23 +0100

    fix: eslint

--- src/utils/innerSize.js
+++ src/utils/innerSize.js
@@ -7,21 +7,21 @@ export function innerHeight(el) {
   if (!style) return el.clientHeight;
   return (
     el.clientHeight -
-    parseInt(style.getPropertyValue("padding-top"), 10) -
-    parseInt(style.getPropertyValue("padding-bottom"), 10)
+    parseInt(style.getPropertyValue('padding-top'), 10) -
+    parseInt(style.getPropertyValue('padding-bottom'), 10)
   );
 }
 
 // Calculate width without padding.
 export function innerWidth(el) {
   if (!el) return 0;
-  
+
   const style = window.getComputedStyle(el, null);
   // Hidden iframe in Firefox returns null, https://github.com/malte-wessel/react-textfit/pull/34
   if (!style) return el.clientWidth;
   return (
     el.clientWidth -
-    parseInt(style.getPropertyValue("padding-left"), 10) -
-    parseInt(style.getPropertyValue("padding-right"), 10)
+    parseInt(style.getPropertyValue('padding-left'), 10) -
+    parseInt(style.getPropertyValue('padding-right'), 10)
   );
 }
